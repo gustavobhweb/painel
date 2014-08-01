ee2b4a162c6e0d1176a6feced11011587b655c1a
hgjg <ijhjk>
2014-08-01 17:47:51 -0300

    Final do desenvolvimento do layout da tela de login

--- app/views/user/login.blade.php
+++ app/views/user/login.blade.php
@@ -7,7 +7,14 @@
 @section("content")
 
     <div id="box-login">
-    {{ Form::open(["route" => "user/login", "autocomplete" => "off"]) }}
+
+    <h4>PAINEL TITÃS - SEJA BEM-VINDO</h4>
+
+    {{ Form::open([
+        "route" => "user/login",
+        "autocomplete" => "off",
+        "class" => "form-horizontal"
+    ]) }}
 
         @if ($error = $errors->first("password"))
             <div class="error">
@@ -15,20 +22,39 @@
             </div><!-- .error -->
         @endif
 
-        {{ Form::label("username", "Usuário:") }}
-        {{ Form::text("username", Input::old("username"), [
-            "class" => "",
-            "placeholder" => "Usuário"
-        ]) }}
+        <div class="form-group">
+            {{ Form::label("username", "Usuário:", [
+                "class" => "col-sm-2 control-label"
+            ]) }}
+            <div class="col-sm-10">
+                {{ Form::text("username", Input::old("username"), [
+                    "class" => "txt-default txt-email",
+                    "required" => "required",
+                    "placeholder" => "exemplo@email.com"
+                ]) }}
+            </div><!-- .col-sm-10 -->
+        </div><!-- .form-group -->
 
-        {{ Form::label("password", "Senha:") }}
-        {{ Form::password("password", [
-            "placeholder" => "Senha"
-        ]) }}
+        <div class="form-group">
+            {{ Form::label("password", "Senha:", [
+                "class" => "col-sm-2 control-label"
+            ]) }}
+            <div class="col-sm-10">
+                {{ Form::password("password", [
+                    "class" => "txt-default txt-pass",
+                    "required" => "required",
+                    "placeholder" => "♥ ♦ ♣ ♠ ♥ ♦ ♣ ♠"
+                ]) }}
+            </div><!-- .col-sm-10 -->
+        </div><!-- .form-group -->
 
-        {{ Form::submit("Entrar") }}
+        {{ Form::submit("Entrar", [
+            "class" => "btn-panel-1 pull-right"
+        ]) }}
 
     {{ Form::close() }}
+
+    <div class="clearfix"></div>
     </div><!-- #box-login -->
 
 @stop
