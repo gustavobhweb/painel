11404b222fcd039a9f2783bfb5b3edd207f92458
Gustavo Carmo <gustavobhweb@gmail.com>
2014-10-16 15:27:05 -0300

    sdf

--- app/models/User.php
+++ app/models/User.php
@@ -1,42 +1,16 @@
 <?php
 
 use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
-
+	
+	use UserTrait, RemindableTrait;
+	
     protected $table = "usuarios";
     protected $hidden = ["password"];
-
-    public function getAuthIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getAuthPassword()
-    {
-        return $this->password;
-    }
-
-    public function getReminderEmail()
-    {
-        return $this->email;
-    }
-
-    public function getRememberToken()
-    {
-        return $this->remenber_token;
-    }
-
-    public function setRememberToken($value)
-    {
-        $this->remember_token = $value;
-    }
-
-    public function getRememberTokenName()
-    {
-        return 'remember_token';
-    }
 
     public static function isValidEmail($email)
     {
